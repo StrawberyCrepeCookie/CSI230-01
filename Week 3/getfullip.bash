4ae8eb6458302097b5b41aa4cce30edc415bf475
Harrison Labrecque <harrison.labrecque@mymail.champlain.edu>
2023-09-25 21:18:28 -0400

    Week 3 FIXED

--- Week 3/getfullip.bash
+++ Week 3/getfullip.bash
@@ -1,1 +1,1 @@
-ip addr | grep 192.168.145.128/24 | cut -d ' ' -f 6
+ip addr | grep "inet .* brd" | cut -d ' ' -f6
--- Week 3/menu.bash
+++ Week 3/menu.bash
@@ -8,27 +8,32 @@ echo "2. Get my ip address in binary"
 echo "3. Get my network mask in binary"
 echo "4. Get my network address in binary"
 echo "5. Convert binary to IP address"
-echo "6. Quit\n:::"
+echo "6. Quit"
 
 read choice
 
 case $choice in 
-	1) 
+	1)
+		echo "Your IP: "
 		bash getip.bash
 	;;
 	2)
+		echo "Your IP in binary: "
 		bash myipbinary.bash
 	;;
 	3)
+		echo "Your network mask in binary: "
 		bash mymaskbinary.bash
 	;;
 	4)
+		echo "Your network address in binary: "
 		bash mynetworkbinary.bash
 	;;
 	5)
 		bash convbin2ip.bash
 	;;
 	6)
+		echo "Exiting...."
 		loop=0
 	;;
 	*)
