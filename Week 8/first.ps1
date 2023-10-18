cbcb801a46f7358052e4d84ab54f4c52e281e499
hlabrecque <hlabrecque@cox.net>
2023-10-18 13:53:15 -0400

    Updated with first.ps1

---
+++ Week 8/first.ps1
@@ -0,0 +1,9 @@
+﻿clear
+
+cd C:\Users\champuser\Desktop\
+
+Get-Eventlog -list
+
+$readLog = read-host -Prompt "Please select a log to review"
+
+Get-EventLog -LogName $readLog -Newest 3 | export-csv -NoTypeInformation -Path "C:\Users\champuser\Desktop\rep.csv"
