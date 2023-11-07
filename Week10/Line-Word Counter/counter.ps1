ef74188b7d74d19b89320dcdcd0122d67281e0c3
hlabrecque <hlabrecque@cox.net>
2023-11-07 18:40:32 -0500

    Week 10 assignments

--- Week10/Line-Word Counter/counter.ps1
+++ Week10/Line-Word Counter/counter.ps1
@@ -4,6 +4,9 @@ $directory = cd "C:\users\champuser\CSI230-01"
 
 $FilesToLookAt = Get-ChildItem -Recurse -Filter $directory
 
-for ($i = 0; $i -lt $FilesToLookAt.Length; $i++) {
-    Get-Content $FilesToLookAt.Length | Measure-Object lines, words
+foreach ($file in $FilesToLookAt) {
+    if ($file.PSIsContainer -eq $false) {
+        Get-Content $file.FullName | Measure-Object -Line -Word
+        
+    }
 }
